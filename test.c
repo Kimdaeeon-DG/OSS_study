45ada8a590496a01b7905568e66b97ffb7603ea4
Daeeon Kim <daeekim@c3r5s4.42gyeongsan.kr>
2024-04-21 19:24:57 +0900

    rb 2~6

--- test.c
+++ test.c
@@ -3,12 +3,6 @@
 int main(void)
 {
 	printf("rebase1 test");
-	printf("rebase1 test");
-	printf("rebase2 test");
-	printf("rebase3 test");
-	printf("rebase4 test");
-	printf("rebase5 test");
-	printf("rebase6 test");
 	return (0);
 }
 
